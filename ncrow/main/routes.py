2bedd33ff52f7c9ac49172d00f1dcda49dd551ff
nodebe <nodywelete3@gmail.com>
2021-08-29 06:49:48 +0100

    added fees page

--- ncrow/main/routes.py
+++ ncrow/main/routes.py
@@ -8,3 +8,8 @@ ERROR = 'Something went wrong, try again later!'
 @main.route('/')
 def index():
 	return render_template('index.html', title='9Crow - Securing every purchase')
+
+@main.route('/fees')
+def fees():
+	
+	return render_template('fees.html', title='9crow - Fees')
